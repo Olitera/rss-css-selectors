2e5f5b418c1d7dc8e535c14fc13c8f48cb635f9e
Olitera <lu_gu@mail.ru>
2023-06-22 14:27:33 +0200

    feat: add lines in css-editor

--- rss-css-selectors/src/modules/css-editor/css-editor.ts
+++ rss-css-selectors/src/modules/css-editor/css-editor.ts
@@ -9,5 +9,18 @@ export class Editor {
 
   constructor() {
     this.element = templateCreation(el) as HTMLElement;
+    this.renderCssEditor();
+  }
+
+  renderCssEditor(): void {
+    const table: Element | null = this.element.querySelector('.editor-table');
+    const numbers = 19;
+    for (let i = 0; i < numbers; i++) {
+      const editorLinea: ChildNode = templateCreation(
+        `<div class="editor-table-linea"><div class="table-linea-number">${i + 2}</div></div>`
+      );
+      console.log(editorLinea);
+      table?.append(editorLinea);
+    }
   }
 }
